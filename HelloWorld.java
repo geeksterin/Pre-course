0b72b219506d947c1a77c6293813950a16eb0c53
SUKAMAL JANA <103117076+SUVA88@users.noreply.github.com>
2022-04-07 09:00:03 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+package april_06_2022;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+
+	      int num1 = 500, num2 = 7000, num3 = 7;
+
+	      if( num1 >= num2 && num1 >= num3)
+	          System.out.println(num1+" is greater");
+
+	      else if (num2 >= num1 && num2 >= num3)
+	          System.out.println(num2+" is greater");
+
+	      else
+	          System.out.println(num3+" is greater");
+	  }
+	}
+
